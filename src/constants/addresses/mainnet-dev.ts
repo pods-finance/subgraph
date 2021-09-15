e3825f306007053064c14e9b676fe67eddbf76e5
Robsonsjre <robsonsjre@gmail.com>
2021-09-15 09:47:36 -0300

    added new mainnet manager

--- src/constants/addresses/mainnet-dev.ts
+++ src/constants/addresses/mainnet-dev.ts
@@ -1,5 +1,5 @@
 export let variant = "mainnet-dev";
 export let dev = true;
-export let network = "kovan";
-export let manager = "0x11dB0eDFe06Cd06C9AFE96b9813043e0853e7926";
-export let startBlock = 25055350;
+export let network = "mainnet";
+export let manager = "0xE4Da64757b2B29dB43429A52CaF7aD884c76f8b0";
+export let startBlock = 13215188;
