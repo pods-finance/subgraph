76966e02a3d2647cb73cb81bed008f6d723d11f5
Robsonsjre <robsonsjre@gmail.com>
2021-11-18 20:13:11 -0300

    fixed goerli startblock

--- src/constants/_generated/goerli.js
+++ src/constants/_generated/goerli.js
@@ -5,4 +5,4 @@ exports.variant = "goerli";
 exports.dev = false;
 exports.network = "goerli";
 exports.manager = "0xD23548bfE7A9F1B7598f40bc6698A2D10091E75B";
-exports.startBlock = 5867837;
+exports.startBlock = 5867000;
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,4 +2,4 @@ export let variant = 'goerli';
 export let dev = false;
 export let network = 'goerli';
 export let manager = '0xD23548bfE7A9F1B7598f40bc6698A2D10091E75B';
-export let startBlock = 5867837;
+export let startBlock = 5867000;
