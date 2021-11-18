739649bbbec3b9b3966e63361d06efc304875030
Robsonsjre <robsonsjre@gmail.com>
2021-11-18 13:03:59 -0300

    added goerli deployment

---
+++ src/constants/_generated/goerli.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.startBlock = exports.manager = exports.network = exports.dev = exports.variant = void 0;
+exports.variant = "goerli";
+exports.dev = false;
+exports.network = "goerli";
+exports.manager = "0xD23548bfE7A9F1B7598f40bc6698A2D10091E75B";
+exports.startBlock = 5867837;
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
-export let variant = 'arbitrum-dev';
-export let dev = true;
-export let network = 'arbitrum-one';
-export let manager = '0x84601612702C7699c09bBF3C033747709F529008';
-export let startBlock = 2988740;
+export let variant = 'goerli';
+export let dev = false;
+export let network = 'goerli';
+export let manager = '0xD23548bfE7A9F1B7598f40bc6698A2D10091E75B';
+export let startBlock = 5867837;
